741782442a25f953cce5e0157d6c14bcd58ab942
wangkai <13824458970@163.com>
2016-09-29 10:48:03 +0800

    嘎嘎嘎

---
+++ LoveFreshBeen-swift/LoveFreshBeen-swift/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  LoveFreshBeen-swift
+//
+//  Created by 汪凯 on 16/9/29.
+//  Copyright © 2016年 汪凯. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
